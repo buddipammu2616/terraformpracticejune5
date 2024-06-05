77a56e5bbcc06273461e50894a6a6fae895eb0c0
mahesh <maheshkuraganti9@gmail.com>
2024-06-05 23:35:31 +0530

    availability zone updated and commited

--- ec2manifest/ec2.tf
+++ ec2manifest/ec2.tf
@@ -1,7 +1,7 @@
 #Resource block
 resource "aws_instance" "maheshec2" {
   ami = "ami-00fa32593b478ad6e"
-  availability_zone = "ap-south-1a"
+  availability_zone = "ap-south-1b"
   instance_type = "t2.micro"
   tags = {
     "Name" = "myec2one"
