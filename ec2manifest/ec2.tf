6e364607352c1be29692f0655caa4fb3b8d23d47
mahesh <maheshkuraganti9@gmail.com>
2024-06-05 18:13:22 +0530

    create ec2 manifest and tf file and commited

---
+++ ec2manifest/ec2.tf
@@ -0,0 +1,9 @@
+#Resource block
+resource "aws_instance" "maheshec2" {
+  ami = "ami-00fa32593b478ad6e"
+  availability_zone = "ap-south-1a"
+  instance_type = "t2.micro"
+  tags = {
+    "Name" = "myvpc1"
+  }
+}
