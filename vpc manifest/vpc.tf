6f0b76ceaa26d71e3cfb9526120835e82f6dd041
mahesh <maheshkuraganti9@gmail.com>
2024-06-05 11:58:28 +0530

    vpc modified and commited

--- vpc manifest/vpc.tf
+++ vpc manifest/vpc.tf
@@ -5,3 +5,9 @@ resource "aws_vpc" "maheshvpc1" {
     "Name" = "maheshvpc1"
       }
 }
+resource "aws_vpc" "maheshvpc2" {
+    cidr_block = "10.1.0.0/16"
+  tags = {
+    "Name" = "maheshvpc2"
+  }
+}
